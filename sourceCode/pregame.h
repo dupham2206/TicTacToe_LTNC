7a0097e703597296e874fc39ade32b0a09f37174
= <=>
2021-04-28 01:30:53 +0700

    add sound 2

---
+++ sourceCode/pregame.h
@@ -0,0 +1,173 @@
+#ifndef PREGAME_H
+#define PREGAME_H
+
+#include<iostream>
+#include "button.h"
+#include "sdlsupport.h"
+
+class PreGame {
+private:
+    pair<int, int> choiceSizeOfBoard[6];
+    int choiceNumberOfPieceConsecutiveToWin[4];
+    int sizeOfChoiceMap = 4;
+    int sizeOfChoicePlayerAI = 2;
+    Button ChoiceMap[6]; // 4 button choice map
+    Button ChoicePlayerAI[4]; // 2 button choose play with AI or play with another player
+    int currentButtonChoosingMap = 0;
+    int currentButtonChoosingPlayerAI = STATE_EMPTY; // Not choose
+    int stateOfBulletinBoard = EMPTY;
+    Button EnterToGame;
+
+    void setUpForInfoOfMap(){
+        choiceSizeOfBoard[MAP_33] = {3 , 3};
+        choiceNumberOfPieceConsecutiveToWin[MAP_33] = 3;
+        choiceSizeOfBoard[MAP_55] = {5 , 5};
+        choiceNumberOfPieceConsecutiveToWin[MAP_55] = 4;
+        choiceSizeOfBoard[MAP_99] = {9 , 9};
+        choiceNumberOfPieceConsecutiveToWin[MAP_99] = 5;
+        choiceSizeOfBoard[MAP_1212] = {12 , 12};
+        choiceNumberOfPieceConsecutiveToWin[MAP_1212] = 5;
+    }
+    void setUpForButtonMap(){
+        for(int i = 1; i <= 2; ++i){
+            for(int j = 1; j <= 2; ++j){
+                int currentSetUp = (i - 1) * 2 + j;
+                ChoiceMap[currentSetUp].setPositionTopLeft(SCREEN_WIDTH / 32 + (j - 1) * SCREEN_WIDTH * 11 / 48, SCREEN_HEIGHT * 7 / 12 + (i - 1) * SCREEN_HEIGHT / 8);
+                ChoiceMap[currentSetUp].setLengthOfButton(SCREEN_WIDTH / 6, SCREEN_HEIGHT / 8);
+                ChoiceMap[currentSetUp].setTypeOfButton(CHOICE_MAP_PREGAME);
+                ChoiceMap[currentSetUp].setStateButton(currentSetUp);
+            }
+        }
+    }
+    void setUpForButtonPlayerAI(){
+        ChoicePlayerAI[STATE_PLAYER].setPositionTopLeft(SCREEN_WIDTH * 89 / 128, SCREEN_HEIGHT * 14 / 24);
+        ChoicePlayerAI[STATE_PLAYER].setLengthOfButton(SCREEN_WIDTH / 6, SCREEN_HEIGHT / 8);
+        ChoicePlayerAI[STATE_PLAYER].setTypeOfButton(CHOICE_PLAYER_AI_PREGAME);
+        ChoicePlayerAI[STATE_PLAYER].setStateButton(STATE_PLAYER);
+        ChoicePlayerAI[STATE_AI].setPositionTopLeft(SCREEN_WIDTH * 89 / 128, SCREEN_HEIGHT * 17 / 24);
+        ChoicePlayerAI[STATE_AI].setLengthOfButton(SCREEN_WIDTH / 6, SCREEN_HEIGHT / 8);
+        ChoicePlayerAI[STATE_AI].setTypeOfButton(CHOICE_PLAYER_AI_PREGAME);
+        ChoicePlayerAI[STATE_AI].setStateButton(STATE_AI);
+    }
+    void setUpForButtonEnterGame(){
+        EnterToGame.setPositionTopLeft(13 * SCREEN_WIDTH / 32, 10 * SCREEN_HEIGHT / 12);
+        EnterToGame.setLengthOfButton(SCREEN_WIDTH * 5 / 24 , SCREEN_WIDTH / 9);
+        EnterToGame.setTypeOfButton(ENTER_PREGAME);
+        EnterToGame.setStateButton(true);
+    }
+    void checkEventForMapButton(SDL_Event &e, SDL_Renderer* renderer){
+        for(int i = 1; i <= sizeOfChoiceMap; ++i){
+            if(ChoiceMap[i].handleEventButton(e, CHOICE_MAP_PREGAME, i, i) == true){ // State k thay doi nen de i va i
+                if(currentButtonChoosingMap)
+                    ChoiceMap[currentButtonChoosingMap].setTypeOfButton(CHOICE_MAP_PREGAME);
+                ChoiceMap[i].setTypeOfButton(CHOICE_MAP_SUCCESS_PREGAME);
+                currentButtonChoosingMap = i;
+                render(renderer);
+            }
+        }
+    }
+    void checkEventForPlayerAIButton(SDL_Event &e, SDL_Renderer* renderer){
+        for(int i = 1; i <= sizeOfChoicePlayerAI; ++i){
+            if(ChoicePlayerAI[i].handleEventButton(e, CHOICE_PLAYER_AI_PREGAME , i, i) == true){
+                if(currentButtonChoosingPlayerAI)
+                    ChoicePlayerAI[currentButtonChoosingPlayerAI].setTypeOfButton(CHOICE_PLAYER_AI_PREGAME);
+                ChoicePlayerAI[i].setTypeOfButton(CHOICE_PLAYER_AI_SUCCESS_PREGAME);
+                currentButtonChoosingPlayerAI = i;
+                render(renderer);
+            }
+        }
+    }
+    void checkEventForEnterButton(SDL_Event &e, SDL_Renderer* renderer, bool &inGame){
+        if(EnterToGame.handleEventButton(e, ENTER_PREGAME, true , true) == true){
+            if(currentButtonChoosingMap && currentButtonChoosingPlayerAI){
+                if(currentButtonChoosingMap == MAP_1212 && currentButtonChoosingPlayerAI == STATE_AI){
+                    Mix_PlayChannel(-1, dataChunk[ERROR], 0);
+                    stateOfBulletinBoard = CAN_NOT_PLAY_MAP1212_WITH_AI;
+                    render(renderer);
+                }
+                else if(currentButtonChoosingMap == MAP_55 && currentButtonChoosingPlayerAI == STATE_AI){
+                    Mix_PlayChannel(-1, dataChunk[ERROR], 0);
+                    stateOfBulletinBoard = CAN_NOT_PLAY_MAP55_WITH_AI;
+                    render(renderer);
+                }
+                else inGame = true;
+            }
+            else if(currentButtonChoosingMap == NOT_CHOOSE || currentButtonChoosingPlayerAI == STATE_EMPTY){
+                Mix_PlayChannel(-1, dataChunk[ERROR], 0);
+                if(currentButtonChoosingMap && !currentButtonChoosingPlayerAI){
+                    stateOfBulletinBoard = NOT_CHOOSE_PLAYER;
+                    render(renderer);
+                }
+                if(currentButtonChoosingPlayerAI && !currentButtonChoosingMap){
+                    stateOfBulletinBoard = NOT_CHOOSE_MAP;
+                    render(renderer);
+                }
+                if(!currentButtonChoosingMap && !currentButtonChoosingPlayerAI){
+                    stateOfBulletinBoard = NOT_CHOOSE_PLAYER_AND_MAP;
+                    render(renderer);
+                }
+            }
+        }
+    }
+
+public:
+    void setUpPreGame(){
+        setUpForInfoOfMap();
+        setUpForButtonMap();
+        setUpForButtonPlayerAI();
+        setUpForButtonEnterGame();
+    }
+    pair<int,int> getChooseSizeOfBoard(){
+        return choiceSizeOfBoard[currentButtonChoosingMap];
+    }
+    int GetChoiceNumberOfPieceConsecutiveToWin(){
+        int chooseMap = currentButtonChoosingMap;
+        currentButtonChoosingMap = STATE_EMPTY;
+        return choiceNumberOfPieceConsecutiveToWin[chooseMap];
+    }
+    int getAIorPlayer(){
+        int chooseAIorPlayer = currentButtonChoosingPlayerAI;
+        currentButtonChoosingPlayerAI = STATE_EMPTY;
+        return chooseAIorPlayer - 1;
+    }
+    void handleEvent(SDL_Window* window, SDL_Renderer* renderer, bool &inGame){
+        SDL_Event e;
+        while( SDL_PollEvent( &e )){
+            if(e.type == SDL_QUIT) quitSDL(window, renderer);
+            if(e.type == SDL_MOUSEBUTTONDOWN){
+                checkEventForMapButton(e, renderer);
+                checkEventForPlayerAIButton(e, renderer);
+                checkEventForEnterButton(e, renderer, inGame);
+            }
+        }
+    }
+    void render(SDL_Renderer* renderer){
+        SDL_RenderClear(renderer);
+        // render choice buttons of map
+        for(int i = 1; i <= sizeOfChoiceMap; ++i)
+            ChoiceMap[i].render(renderer, false, false);
+        // render choice buttons of AI and Player
+        for(int i = 1; i <= sizeOfChoicePlayerAI; ++i)
+            ChoicePlayerAI[i].render(renderer, false, false);
+        // render enter button
+        EnterToGame.render(renderer, false, false);
+        // render title of game
+        RenderMedia(renderer, dataImageButton[HEADING_PREGAME][1],
+                    SCREEN_WIDTH / 8, 0, SCREEN_WIDTH/ 2, SCREEN_HEIGHT / 3);
+        // render title of choose map
+        RenderMedia(renderer, dataImageButton[CHOOSE_MAP_PREGAME][1],
+                    SCREEN_WIDTH / 16, SCREEN_HEIGHT * 5 / 12, SCREEN_WIDTH * 5 / 16, SCREEN_HEIGHT / 6);
+        // render title of choose AI and Player
+        RenderMedia(renderer, dataImageButton[CHOOSE_AI_PREGAME][1],
+                    SCREEN_WIDTH * 10 / 16, SCREEN_HEIGHT * 5 / 12, SCREEN_WIDTH * 5 / 16, SCREEN_HEIGHT / 6);
+        // render bulletin board
+        RenderMedia(renderer, dataImageButton[BULLETIN_BOARD_PREGAME][stateOfBulletinBoard],
+                    SCREEN_WIDTH * 3 / 4, 0, SCREEN_WIDTH/ 4, SCREEN_HEIGHT / 3);
+        SDL_RenderPresent(renderer);
+
+    }
+};
+
+
+
+#endif
---
+++ sourceCode/sdlsupport.h
@@ -0,0 +1,199 @@
+#ifndef SDLSUPPORT_H
+#define SDLSUPPORT_H
+
+#include<iostream>
+#include "SDL_mixer.h"
+using namespace std;
+
+SDL_Texture* loadMedia(SDL_Renderer* Renderer, string path){
+    SDL_Texture* gTexture;
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str());
+    if( loadedSurface == NULL ){
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+    }
+    else{
+        gTexture = SDL_CreateTextureFromSurface(Renderer, loadedSurface);
+        if( gTexture == NULL ){
+        printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+        }
+        SDL_FreeSurface( loadedSurface );
+    }
+    return gTexture;
+}
+void RenderMedia(SDL_Renderer* renderer, SDL_Texture* gTexture, int topX, int topY, int width, int height){
+    SDL_Rect Dest;
+    Dest.x = topX + 1;
+    Dest.y = topY + 1;
+    Dest.w = width - 1;
+    Dest.h = height - 1;
+    SDL_RenderCopy( renderer, gTexture, NULL, &Dest );
+}
+
+void logSDLError(std::ostream& os, const std::string &msg, bool fatal){
+    os << msg << " Error: " << SDL_GetError() << std::endl;
+    if (fatal) {
+        SDL_Quit();
+        exit(1);
+    }
+}
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer, int SCREEN_WIDTH, int SCREEN_HEIGHT, string WINDOW_TITLE){
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logSDLError(std::cout, "SDL_Init", true);
+
+    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    //window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+    //SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
+
+
+    //Khi thông thường chạy với môi trường bình thường ở nhà
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+                                          SDL_RENDERER_PRESENTVSYNC);
+    //Khi chạy ở máy thực hành WinXP ở trường (máy ảo)
+    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+    if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+    int imgFlags = IMG_INIT_PNG;
+    if( !( IMG_Init( imgFlags ) & imgFlags ) )
+        printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+}
+
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer){
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
+}
+
+SDL_Texture* dataImageButton[20][20];
+Mix_Chunk* dataChunk[10];
+Mix_Music* dataMusic[10];
+static const int SCREEN_WIDTH = 800;
+static const int SCREEN_HEIGHT = 600;
+static const string WINDOW_TITLE = "TICTACTOE GAME";
+static const int directX[10] = {1, 1, 0, 1, 0, -1, -1, -1};
+static const int directY[10] = {1, 0, 1, -1, -1, 1, 0, -1};
+
+enum typeOfImage
+{
+    ENTER_PREGAME,
+    CHOICE_MAP_PREGAME,
+    CHOICE_MAP_SUCCESS_PREGAME,
+    SQUARE_GAME,
+    SQUARE_GAME_HAVEPLAYED,
+    RESTART_GAME,
+    RETURN_MENU_GAME,
+    CHOICE_PLAYER_AI_PREGAME,
+    CHOICE_PLAYER_AI_SUCCESS_PREGAME,
+    CHOOSE_AI_PREGAME,
+    CHOOSE_MAP_PREGAME,
+    HEADING_PREGAME,
+    PAUSE_GAME,
+    BULLETIN_BOARD_PREGAME,
+    ENDGAME_GAME
+};
+
+enum stateOfReporter {
+    EMPTY,
+    NOT_CHOOSE_PLAYER_AND_MAP,
+    NOT_CHOOSE_PLAYER,
+    NOT_CHOOSE_MAP,
+    CAN_NOT_PLAY_MAP1212_WITH_AI,
+    CAN_NOT_PLAY_MAP55_WITH_AI,
+    YOUR_TURN,
+    AI_TURN,
+    PLAYER1_TURN,
+    PLAYER2_TURN,
+    ENDGAME,
+    ENDGAME_ADMIT_LOSE,
+};
+
+enum stateOfEndGameBULLETINBOARD {
+    YOU_WIN,
+    YOU_LOSE,
+    DRAW,
+    PLAYER1_WIN,
+    PLAYER2_WIN
+};
+
+enum stateOfaSquare {
+    STATE_EMPTY,
+    STATE_PLAYER,
+    STATE_AI
+};
+
+enum stateButtonMap {
+    NOT_CHOOSE,
+    MAP_33,
+    MAP_55,
+    MAP_99,
+    MAP_1212
+};
+
+enum stateOfPlayer {
+    NOT_ENDGAME,
+    FIRST_PLAYER,
+    SECOND_PLAYER,
+    TWO_PLAYER_DRAW,
+};
+enum chunk {
+    CLICK,
+    ENDGAME_MUSIC,
+    ERROR
+
+};
+void setData(SDL_Renderer* renderer){
+    dataImageButton[ENTER_PREGAME][1] = loadMedia(renderer , "../pictures/EnterGameButton.png");
+    dataImageButton[CHOICE_MAP_PREGAME][MAP_33] = loadMedia(renderer , "../pictures/choice33.png");
+    dataImageButton[CHOICE_MAP_PREGAME][MAP_55] = loadMedia(renderer , "../pictures/choice55.png");
+    dataImageButton[CHOICE_MAP_PREGAME][MAP_99] = loadMedia(renderer , "../pictures/choice99.png");
+    dataImageButton[CHOICE_MAP_PREGAME][MAP_1212] = loadMedia(renderer , "../pictures/choice1212.png");
+    dataImageButton[CHOICE_MAP_SUCCESS_PREGAME][MAP_33] = loadMedia(renderer , "../pictures/choice33success.png");
+    dataImageButton[CHOICE_MAP_SUCCESS_PREGAME][MAP_55] = loadMedia(renderer , "../pictures/choice55success.png");
+    dataImageButton[CHOICE_MAP_SUCCESS_PREGAME][MAP_99] = loadMedia(renderer , "../pictures/choice99success.png");
+    dataImageButton[CHOICE_MAP_SUCCESS_PREGAME][MAP_1212] = loadMedia(renderer , "../pictures/choice1212success.png");
+    dataImageButton[CHOICE_PLAYER_AI_SUCCESS_PREGAME][STATE_PLAYER] = loadMedia(renderer , "../pictures/choicePlayPlayerSuccess.png");
+    dataImageButton[CHOICE_PLAYER_AI_SUCCESS_PREGAME][STATE_AI] = loadMedia(renderer , "../pictures/choicePlayAISuccess.png");
+    dataImageButton[CHOICE_PLAYER_AI_PREGAME][STATE_PLAYER] = loadMedia(renderer , "../pictures/choicePlayPlayer.png");
+    dataImageButton[CHOICE_PLAYER_AI_PREGAME][STATE_AI] = loadMedia(renderer , "../pictures/choicePlayAI.png");
+    dataImageButton[SQUARE_GAME][FIRST_PLAYER] = loadMedia(renderer , "../pictures/player1_X.png");
+    dataImageButton[SQUARE_GAME][SECOND_PLAYER] = loadMedia(renderer , "../pictures/player2_O.png");
+    dataImageButton[SQUARE_GAME_HAVEPLAYED][FIRST_PLAYER] = loadMedia(renderer , "../pictures/player1_X_havePlayed.png");
+    dataImageButton[SQUARE_GAME_HAVEPLAYED][SECOND_PLAYER] = loadMedia(renderer , "../pictures/player2_O_havePlayed.png");
+    dataImageButton[RESTART_GAME][1] = loadMedia(renderer, "../pictures/RestartGame.png");
+    dataImageButton[RETURN_MENU_GAME][1] = loadMedia(renderer, "../pictures/returnMenu.png");
+    dataImageButton[CHOOSE_AI_PREGAME][1] = loadMedia(renderer, "../pictures/chooseAI.png");
+    dataImageButton[CHOOSE_MAP_PREGAME][1] = loadMedia(renderer, "../pictures/chooseMap.png");
+    dataImageButton[HEADING_PREGAME][1] = loadMedia(renderer, "../pictures/heading.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][EMPTY] = loadMedia(renderer, "../pictures/bulletinBoard.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][NOT_CHOOSE_PLAYER_AND_MAP] = loadMedia(renderer, "../pictures/bulletinBoard_chooseAIandMap.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][NOT_CHOOSE_PLAYER] = loadMedia(renderer, "../pictures/bulletinBoard_chooseAI.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][NOT_CHOOSE_MAP] = loadMedia(renderer, "../pictures/bulletinBoard_chooseMap.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][CAN_NOT_PLAY_MAP1212_WITH_AI] = loadMedia(renderer, "../pictures/bulletinBoard_notSupportAIwith1212.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][CAN_NOT_PLAY_MAP55_WITH_AI] = loadMedia(renderer, "../pictures/bulletinBoard_notSupportAIwith55.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][YOUR_TURN] = loadMedia(renderer, "../pictures/bulletinBoard_yourTurn.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][AI_TURN] = loadMedia(renderer, "../pictures/bulletinBoard_AIturn.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][PLAYER1_TURN] = loadMedia(renderer, "../pictures/bulletinBoard_turnPlayer1.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][PLAYER2_TURN] = loadMedia(renderer, "../pictures/bulletinBoard_turnPlayer2.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][ENDGAME] = loadMedia(renderer, "../pictures/bulletinBoard_endGame.png");
+    dataImageButton[BULLETIN_BOARD_PREGAME][ENDGAME_ADMIT_LOSE] = loadMedia(renderer, "../pictures/bulletinBoard_endGameAdmit.png");
+    dataImageButton[ENDGAME_GAME][YOU_WIN] = loadMedia(renderer, "../pictures/bulletinBoard_youWin.png");
+    dataImageButton[ENDGAME_GAME][YOU_LOSE] = loadMedia(renderer, "../pictures/bulletinBoard_youLose.png");
+    dataImageButton[ENDGAME_GAME][DRAW] = loadMedia(renderer, "../pictures/bulletinBoard_draw.png");
+    dataImageButton[ENDGAME_GAME][PLAYER1_WIN] = loadMedia(renderer, "../pictures/bulletinBoard_player1win.png");
+    dataImageButton[ENDGAME_GAME][PLAYER2_WIN] = loadMedia(renderer, "../pictures/bulletinBoard_player2win.png");
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) == -1){
+        printf("%s", Mix_GetError());
+    }
+    dataChunk[CLICK] = Mix_LoadWAV("../sounds/click.wav");
+    dataChunk[ENDGAME_MUSIC] = Mix_LoadWAV("../sounds/endGame.wav");
+    dataChunk[ERROR] = Mix_LoadWAV("../sounds/error.wav");
+
+}
+
+#endif
+
